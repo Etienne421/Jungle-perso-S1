f619cd3fdeb2e45bfd10458a7db259b5a7414e7e
Etienne <1849654@cegepmontpetit.ca>
2022-08-26 10:39:11 -0400

    Création Projet
    Création du Web, DataAccess et DbContext

---
+++ Jungle/Jungle_DataAccess/Data/JungleDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Jungle_DataAccess.Data
+{
+    public class JungleDbContext : DbContext
+    {
+        public JungleDbContext(DbContextOptions<JungleDbContext> options) : base(options)
+        {
+
+        }
+
+
+    }
+}
