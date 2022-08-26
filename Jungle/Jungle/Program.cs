e43e4a91eccd0a38ff2a0254962ced077fdd667d
Etienne <1849654@cegepmontpetit.ca>
2022-08-26 11:00:19 -0400

    Repository et Db création

--- Jungle/Jungle/Program.cs
+++ Jungle/Jungle/Program.cs
@@ -1,9 +1,13 @@
 using Jungle_DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<JungleDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
 var app = builder.Build();
 
---
+++ Jungle/Jungle_DataAccess/Repository/RepositoryAsync.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Jungle_DataAccess.Data;
+using Jungle_DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jungle_DataAccess.Repository
+{
+    public class RepositoryAsync<T> : IRepositoryAsync<T> where T : class
+    {
+        // pour accéder à la BD, aux entités
+        private readonly JungleDbContext _db;
+        // pour faire des changements directs
+        internal DbSet<T> dbset;
+
+        public RepositoryAsync(JungleDbContext db)
+        {
+            _db = db;
+            this.dbset = _db.Set<T>();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await dbset.AddAsync(entity);
+        }
+
+
+    }
+}
