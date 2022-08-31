29d745d31b42e6f928d178e8deacdd9eee97dda1
Etienne0 <1849654@cegepmontpetit.ca>
2022-08-31 09:49:41 -0400

    UnitOfWork et Models

---
+++ Jungle/Jungle_DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿
+using Jungle_DataAccess.Repository.IRepository;
+using Jungle_DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jungle_DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly JungleDbContext _db;
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
